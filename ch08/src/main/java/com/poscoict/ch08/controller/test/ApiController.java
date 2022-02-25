2bbdf65383b1f42031e3726601eba54631793785
fascinate98 <fascinate982@naver.com>
2022-02-25 16:56:53 +0900

    ajax #03

---
+++ ch08/src/main/java/com/poscoict/ch08/controller/test/ApiController.java
@@ -0,0 +1,28 @@
+package com.poscoict.ch08.controller.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.poscoict.ch08.controller.vo.GuestbookVo;
+
+@Controller("apiTestController")
+@RequestMapping("/test")
+public class ApiController {
+
+	@RequestMapping("/text")
+	public String text() {
+		return "text";
+	}
+	
+	@RequestMapping("/html")
+	public String html() {
+		return "html";
+	}
+	
+	@RequestMapping("/json01")
+	public String json01() {
+		return "json01";
+	}
+	
+
+}
