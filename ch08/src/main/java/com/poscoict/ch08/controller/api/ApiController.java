4674cfea6dc519d77818e4622305de11ebbb1a51
fascinate98 <fascinate982@naver.com>
2022-02-28 15:17:02 +0900

    ajax #9

--- ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -1,11 +1,13 @@
 package com.poscoict.ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscoict.ch08.controller.dto.JsonResult;
+import com.poscoict.ch08.controller.dto.XmlResult;
 import com.poscoict.ch08.controller.vo.GuestbookVo;
 
 @Controller
@@ -21,19 +23,53 @@ public class ApiController {
 	@ResponseBody
 	@RequestMapping("/html")
 	public String html() {
-		return "<h1>AJAX 연습</h1> <p>HTML 데이터</p>";
+		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/json" , method = RequestMethod.GET)
+	@RequestMapping("/xml")
+	public Object xml() {
+		XmlResult.GuestbookVo vo = new XmlResult.GuestbookVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("호이~");
+		
+		return XmlResult.success(vo);
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/json", method=RequestMethod.GET)
 	public Object json() {
 		GuestbookVo vo = new GuestbookVo();
 		vo.setNo(1L);
 		vo.setName("둘리");
-		vo.setMessage("호이");
+		vo.setMessage("호이~");
 		
 		return JsonResult.success(vo);
-		//return JsonResult.fail("Exception");
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/post01", method=RequestMethod.POST)
+	public JsonResult post01(GuestbookVo vo) {
+		System.out.println(vo);
+		
+		// service -> respository: DB insert 성공한 후,
+		vo.setNo(1L);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/post02", method=RequestMethod.POST)
+	public JsonResult post02(@RequestBody GuestbookVo vo) {
+		System.out.println(vo);
+		
+		// service -> respository: DB insert 성공한 후,
+		vo.setNo(1L);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
 	}
 	
 }
